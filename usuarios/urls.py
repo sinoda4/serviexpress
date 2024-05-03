3474f6277897217424d42126972ba26062a7d9e7
sinoda4 <sinoda.dev@gmail.com>
2024-05-02 23:27:57 -0400

    trato de agregar transbank

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("login_usuario", views.login_user, name="login"),
     path("logout_usuario", views.logout_user, name="logout"),
     path("registro_usuario", views.register_user, name="registro_usuario"),
+    path("transbank", views.transbank, name="transbank")
 ] 
--- usuarios/views.py
+++ usuarios/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
+from django.http import JsonResponse
 from .forms import *
+import requests
 # Create your views here.
 
 def login_user(request):
@@ -40,3 +42,26 @@ def register_user(request):
     else:
         form  = RegistroUsuarioForm()
         return render(request, 'auth/registroUsuario.html', {"form": form})
+
+def transbank(request):
+    api_key = "579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C"
+    commerce_code = "597055555532"
+    base_url = "https://webpay3gint.transbank.cl"
+    if request.method == "POST":
+        payload = {
+            "buy_order": "ordenCompra12345678",
+            "session_id": "sesion1234557545",
+            "amount": 10000,
+        }
+
+        response = requests.post(f'{base_url}/rswebpaytransaction/api/webpay/v1.2/transactions/', json=payload, headers={'Tbk-Api-Key-Id': commerce_code, 'Tbk-Api-Key-Secret': api_key})
+        if response.status_code == 200:
+            data = response.json()
+            print("funciona")
+            return JsonResponse({'response': data})
+        else:
+            return JsonResponse({'error': 'Error en la solicitud a la API de Transbank'}, status=500)
+        
+        return redirect('transbank')
+    else:
+        return render(request, 'auth/transbank.html', {})
